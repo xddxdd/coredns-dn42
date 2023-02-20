2a4ce1397366fca4554bc33eb844873444d23625
Lan Tian <xuyh0120@outlook.com>
2023-02-19 21:25:04 -0800

    Add test for generateIPv4CNAMERecord

---
+++ ipv4_rev_test.go
@@ -0,0 +1,30 @@
+package dn42
+
+import (
+	"fmt"
+	"net"
+	"testing"
+
+	"github.com/coredns/coredns/plugin/test"
+)
+
+func testIPv4CNAMERecord(t *testing.T, ipString string, mask int, expected string) {
+	x := DN42{
+		DN42RegistryPath: "/",
+		Ttl:              3600,
+		Next:             test.ErrorHandler(),
+	}
+
+	ip := net.ParseIP(ipString)
+	_, cidr, _ := net.ParseCIDR(fmt.Sprintf("%s/%d", ip, mask))
+	_, newQname, _ := x.generateIPv4CNAMERecord("", ip, *cidr)
+	if newQname != expected {
+		t.Errorf("newQname (%s) != expected (%s)", newQname, expected)
+	}
+}
+
+func TestIPv4CNAMERecord(t *testing.T) {
+	for i := 0; i <= 32; i++ {
+		testIPv4CNAMERecord(t, "192.168.0.1", i, fmt.Sprintf("1/%d.0.168.192.in-addr.arpa.", i))
+	}
+}
