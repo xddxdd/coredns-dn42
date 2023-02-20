40486c5de9be20a598e7232dc8114ceded3728ea
Lan Tian <xuyh0120@outlook.com>
2023-02-19 18:30:18 -0800

    Initial commit (copy example plugin)

---
+++ setup_test.go
@@ -0,0 +1,21 @@
+package dn42
+
+import (
+	"testing"
+
+	"github.com/coredns/caddy"
+)
+
+// TestSetup tests the various things that should be parsed by setup.
+// Make sure you also test for parse errors.
+func TestSetup(t *testing.T) {
+	c := caddy.NewTestController("dns", `dn42`)
+	if err := setup(c); err != nil {
+		t.Fatalf("Expected no errors, but got: %v", err)
+	}
+
+	c = caddy.NewTestController("dns", `dn42 more`)
+	if err := setup(c); err == nil {
+		t.Fatalf("Expected errors, but got: %v", err)
+	}
+}
